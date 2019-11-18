fc47b2d72d44fbfa1e3e15cffd65b6cb30cfb958
e <1342464229@qq.com>
2019-11-18 18:11:13 +0800

    v1

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+// import Home from '../views/Home.vue'
+import Login from '../views/login/index'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    name: 'Login',
+    component: Login
   },
   {
     path: '/about',
