4f59eb66e02e09213f4f095f8d8c4f50eb9ff74c
zqiux <1342464229@qq.com>
2019-11-19 18:18:23 +0800

    11.19

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 // import Home from '../views/Home.vue'
-import Login from '../views/login/index'
+// import Login from '../views/login/index'
+// import List from '../views/list/index'
+import routes from './routes'
 
 Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/',
-    name: 'Login',
-    component: Login
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
 
 const router = new VueRouter({
   routes
--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    // 放全局参数
+    goods: []
+  },
+  getters: {
+    // get方法
+    goods: state => state.goods
   },
   mutations: {
+    // 这里放set方法
+    setGoods (state, { goods }) {
+      state.goods = goods
+    }
   },
   actions: {
+
   },
   modules: {
+    // 这里是我自己理解的是为了给全局变量分组，所以需要写提前声明其他store文件，然后引入这里
   }
 })
